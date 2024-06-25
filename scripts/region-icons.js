5886d3475ec59bb943323dd95dbfc6699c5c03ec
farling42 <github@amusingtime.uk>
2024-06-25 14:09:24 +0100

    Better ICON handling

--- scripts/region-icons.js
+++ scripts/region-icons.js
@@ -4,20 +4,6 @@
 // Draw the region in the centre of each of the region's polygons.
 //
 
-/*
-CONFIG.RegionBehavior.typeIcons[this.document.type];
-CONFIG.RegionBehavior.
-
-CONFIG.Region.documentClass = RegionDocument;
-CONFIG.Region.layerClass = RegionLayer;
-CONFIG.Region.objectClass = Region;
-CONFIG.Region.sheetClasses = { base: { "core.RegionConfig": { cls: RegionConfig } } };  // with other stuff too
-CONFIG.Region.typeLabels[behaviourType] = localization.key;
-
-class Region extends PlaceableObject;
-class RegionLayer extends PlaceablesLayer;
-*/
-
 const MODULE_NAME = "easy-regions";
 const IMG_FIELD = "flags.easy-regions.img";
 
@@ -25,89 +11,157 @@ let icon_field;
 
 function iconField() {
   if (icon_field) return icon_field;
+
+  const fields = foundry.data.fields;
   // Create a fake DataField for the image (generic)
-  const options = {
-    name: `${MODULE_NAME}.img`,
-    categories: ["IMAGE"],
-    label: `${MODULE_NAME}.regionConfig.icon.label`,
-    hint: `${MODULE_NAME}.regionConfig.icon.hint`
-  }
-  const context = {
-    name: IMG_FIELD
-  }
-  icon_field = new foundry.data.fields.FilePathField(options, context);
+
+  icon_field = new fields.SchemaField({
+    src: new fields.FilePathField({
+      categories: ["IMAGE"],
+      label: `${MODULE_NAME}.regionConfig.src.label`,
+      hint: `${MODULE_NAME}.regionConfig.src.hint`
+    }),
+    tint: new fields.ColorField({
+      nullable: false,
+      initial: "#ffffff",
+      label: `${MODULE_NAME}.regionConfig.tint.label`,
+      hint: `${MODULE_NAME}.regionConfig.tint.hint`
+    }),
+    size: new fields.NumberField({
+      // Same settings as per BaseNote
+      required: true,
+      nullable: false,
+      integer: true,
+      min: 32,
+      initial: 40,
+      label: `${MODULE_NAME}.regionConfig.size.label`,
+      hint: `${MODULE_NAME}.regionConfig.size.hint`,
+      validationError: `${MODULE_NAME}.regionConfig.size.validation`,
+      label: "NOTE.IconSize"
+    }),
+  }, {
+    // Options
+  }, {
+    // Context
+    name: `flags.${MODULE_NAME}`
+  })
   return icon_field;
 }
 
 
 Hooks.on("renderRegionConfig", (doc, html) => {
-  console.log(`>>> renderRegionConfig`, { doc, html })
-  let section = html.querySelector('section.region-shapes');
-
   // Create the FormGroup to add to the form (specific)
-  const groupConfig = {
-    localize: true
-  };
-  const inputConfig = {
+  const flags = doc.document.flags[MODULE_NAME];
+  const fields = iconField().fields;
+  const group = document.createElement("fieldset");
+  group.append(fields.src.toFormGroup({ localize: true }, {
     placeholder: `${MODULE_NAME}.regionConfig.icon.placeholder`,
-    value: doc.document.flags[MODULE_NAME]?.img,
+    value: flags?.src,
     localize: true,
-  }
-  const formGroup = iconField().toFormGroup(groupConfig, inputConfig);
-  section.append(formGroup);
+  }));
+  group.append(fields.tint.toFormGroup({ localize: true }, {
+    value: flags?.tint,
+    localize: true,
+  }));
+  group.append(fields.size.toFormGroup({ localize: true }, {
+    value: flags?.size,
+    localize: true,
+  }));
+
+  let section = html.querySelector('section.region-shapes');
+  section.append(group);
 })
 
 
 Hooks.on("refreshRegion", async (region, options) => {
-  // region = Region
-  // option.refreshState (boolean)   Region._refreshState()
-  // option.refreshBorder (boolean)  Region._refreshBorder()
-  console.log(`refreshRegion:`, { region, options })
-  if (options.refreshState) {
+  if (!options.refreshState) return;
 
-    //const {texture, iconSize} = region.document;
-    const texture = {
-      src: region.document.flags[MODULE_NAME]?.img,
-      tint: 0xFFFFFF
-    }
-    const iconSize = 32;
+  const texture = region.document.flags[MODULE_NAME]?.src;
 
-    if (region.icons && !texture.src) {
-      region.icons.destroy({ children: true })
-    }
-    if (!texture.src) return;
+  // Initial creation of required icons.
+  // All other changes made during _onUpdate.
+  if (region.icons || !texture) return;
 
-    if (!region.icons) {
-      region.icons = region.addChild(new PIXI.Graphics());
-      region.icons.eventMode = "none";
-    }
-    if (!region.iconTexture || region.iconTextureSrc != texture.src) {
-      region.iconTextureSrc = texture.src;
-      region.iconTexture = await loadTexture(texture.src);
-      for (const icon of region.icons.children)
-        icon.texture = region.iconTexture;
-    }
+  console.log(`${MODULE_NAME} - refreshRegion: creating initial Icons:`, { region, options })
+  const iconTint = region.document.flags[MODULE_NAME]?.tint ?? 0xFFFFFF;
+  const iconSize = region.document.flags[MODULE_NAME]?.size ?? 32;
 
-    // Get the correct number of icons
+  region.icons = region.addChild(new PIXI.Graphics());
+  region.icons.eventMode = "none";
+
+  region.iconTextureSrc = texture;
+  region.iconTexture = await loadTexture(texture);
+
+  for (const node of region.polygonTree) {
+    const icon = new PIXI.Sprite();
+    icon.texture = region.iconTexture;
+    icon.tint = iconTint;
+    icon.width = icon.height = iconSize;
+    icon.x = node.bounds.center.x - (iconSize / 2);
+    icon.y = node.bounds.center.y - (iconSize / 2);
+    region.icons.addChild(icon);
+  }
+})
+
+
+Hooks.on("updateRegion", async (document, changed, options, userId) => {
+  console.log(`${MODULE_NAME} - updateRegion`, { document, changed, options, userId} )
+  const region = document.object;
+
+  const regionFlags = document.flags[MODULE_NAME];
+  if (!regionFlags || !region.icons) return;
+
+  const changedFlags = changed.flags?.[MODULE_NAME];
+  const update_texture = (changedFlags && "src" in changedFlags);
+  const update_tint = (changedFlags && "tint" in changedFlags);
+  const update_size = (changedFlags && "size" in changedFlags);
+  const update_border = ("shapes" in changed);
+
+  console.log(`${MODULE_NAME} - updateRegion`, { update_texture, update_tint, update_size, update_border })
+
+  if (update_texture) {
+    if (region.textureSrc) {
+      console.log(`${MODULE_NAME} - updateRegion: deleting old texture`)
+      region.iconTextureSrc = null;
+      region.iconTexture.destroy();
+      region.iconTexture = null;
+    }
+    if (changedFlags.src) {
+      console.log(`${MODULE_NAME} - updateRegion: creating new texture`)
+      region.iconTextureSrc = changedFlags.src;
+      region.iconTexture = await loadTexture(changedFlags.src);
+      for (const node of region.icons)
+        node.texture = region.iconTexture;
+    }
+  }
+  const iconTint = changedFlags?.tint ?? regionFlags.tint ?? 0xFFFFFF;
+  const iconSize = changedFlags?.size ?? regionFlags.size ?? 32;
+  if (update_border) {
     const oldCount = region.icons.children.length;
     const newCount = region.polygonTree.children.length;
-    for (let i=oldCount; i > newCount; i--)
-      region.icons.children[i-1].destroy({children:true})
-    for (let i=oldCount; i<newCount; i++) {
-      const icon = new PIXI.Sprite(); //ControlIcon({ texture: texture.src, size: iconSize, tint: texture.tint }));
+    console.log(`${MODULE_NAME} - updateRegion: changing border length from ${oldCount} to ${newCount}`)
+    for (let i = oldCount; i > newCount; i--)
+      region.icons.children[i - 1].destroy({ children: true })
+    for (let i = oldCount; i < newCount; i++) {
+      const icon = new PIXI.Sprite();
+      icon.tint = iconTint;
       icon.texture = region.iconTexture;
       icon.width = icon.height = iconSize;
       region.icons.addChild(icon);
     }
-
-    let iconCount=0;
+  }
+  if (update_tint || update_size || update_border) {
+    console.log(`${MODULE_NAME} - updateRegion: updating each icon`)
+    let iconCount = 0;
     for (const node of region.polygonTree) {
-      //console.log(node);
       const icon = region.icons.children[iconCount++];
-      //icon.texture = texture.src;
-      icon.x = node.bounds.center.x - (iconSize / 2);
-      icon.y = node.bounds.center.y - (iconSize / 2);
-      console.log(`ICON '${texture.src}' at (${icon.x}, ${icon.y}) for REGION bounds`, node.bounds)
+      if (update_tint) icon.tint = iconTint;
+      if (update_size || update_border) {
+        if (update_size)
+          icon.width = icon.height = iconSize;
+        icon.x = node.bounds.center.x - (iconSize / 2);
+        icon.y = node.bounds.center.y - (iconSize / 2);
+      }
     }
   }
 })
