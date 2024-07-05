8fb8bf7f154e7a9d628002a8c6cfce68935fa28e
farling42 <github@amusingtime.uk>
2024-07-05 17:15:37 +0100

    Tidy up UUID list generation

--- scripts/region-uuids.js
+++ scripts/region-uuids.js
@@ -15,54 +15,48 @@ import { MOD, SETTING_ONLY_NAV_SCENES } from './easy-regions.js';
 const REGION_DATALIST_NAME = "region-uuids";
 const SPACING = " \u{2794} ";
 
-function sorted(collection) {
-  return [...collection].sort((a, b) => a.name.compare(b.name));
-}
-function addOption(datalist, doc, label) {
-  const option = document.createElement('option');
-  option.value = doc.uuid;
-  option.label = label || doc.name;
-  datalist.append(option);
-}
-
 function my_HTMLDocumentTagsElement_buildElements(wrapper) {
-  let result = wrapper();
+  const result = wrapper();
 
   let datalist;
-  let only_nav_scenes = game.settings.get(MOD.id, SETTING_ONLY_NAV_SCENES);
+  function addOption(doc, label) {
+    if (!datalist) datalist = document.createElement('datalist');
+    const option = document.createElement('option');
+    option.value = doc.uuid;
+    option.label = label || doc.name;
+    datalist.append(option);
+  }
+  function sorted(collection) {
+    return [...collection].sort((a, b) => a.name.compare(b.name));
+  }
+  function scenes() {
+    return sorted(game.settings.get(MOD.id, SETTING_ONLY_NAV_SCENES) ? game.scenes.filter(scene => scene.navigation) : game.scenes);
+  }
 
   const type = this.getAttribute("type");
   if (CONFIG.debug[MOD.id]) console.debug(`${MOD.title} | buildElements: ${type}`);
-  if (type === 'Region') {
-    for (const scene of sorted(game.scenes)) {
-      if (!only_nav_scenes || scene.navigation)
-        for (const region of sorted(scene.regions)) {
-          if (!datalist) datalist = document.createElement('datalist');
-          addOption(datalist, region, `${scene.name}${SPACING}${region.name}`);
-        }
-    }
-  } else if (type === 'Macro') {
-    for (const macro of sorted(game.macros)) {
-      if (!datalist) datalist = document.createElement('datalist');
-      addOption(datalist, macro);
-    }
-  } else if (type === 'Scene') {
-    for (const scene of sorted(game.scenes)) {
-      if (!only_nav_scenes || scene.navigation) {
-        if (!datalist) datalist = document.createElement('datalist');
-        addOption(datalist, scene);
-      }
-    }
-  } else if (type === "RegionBehavior") {
-    for (const scene of sorted(game.scenes)) {
-      if (!only_nav_scenes || scene.navigation)
-        for (const region of sorted(scene.regions)) {
-          for (const behavior of sorted(region.behaviors)) {
-            if (!datalist) datalist = document.createElement('datalist');
-            addOption(datalist, behavior, `${scene.name}${SPACING}${region.name}${SPACING}${behavior.name}`);
-          }
-        }
-    }
+
+  switch (type) {
+    case "Region":
+      scenes().forEach(scene =>
+        sorted(scene.regions).forEach(region =>
+          addOption(region, `${scene.name}${SPACING}${region.name}`)));
+      break;
+
+    case "Macro":
+      sorted(game.macros).forEach(macro => addOption(macro));
+      break;
+
+    case "Scene":
+      scenes().forEach(scene => addOption(scene));
+      break;
+
+    case "RegionBehavior":
+      scenes().forEach(scene =>
+        sorted(scene.regions).forEach(region =>
+          sorted(region.behaviors).forEach(behavior =>
+            addOption(behavior, `${scene.name}${SPACING}${region.name}${SPACING}${behavior.name}`))));
+      break;
   }
 
   if (datalist) {
